05b8dde7b21d5af572dc2604cb92943f89358f17
marcingoryl <33161@student.pwsztar.edu.pl>
2022-03-21 16:34:17 +0100

    swagger documentation created

--- src/main/java/pl/edu/pwsztar/Application.java
+++ src/main/java/pl/edu/pwsztar/Application.java
@@ -2,11 +2,46 @@ package pl.edu.pwsztar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @Bean
+    public Docket get(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("pl.edu.pwsztar.controller"))
+                .build().apiInfo(createApiInfo());
+    }
+
+    private ApiInfo createApiInfo(){
+        return new ApiInfo("Movies API",
+                "API created for describing movies",
+                "1.00",
+                "http://localhost:63342/master/movies/frontend/movies.html",
+                new Contact("Marcin", "", "macingoryl99@o2.pl"),
+                "my own licence",
+                "http://localhost:63342/master/movies/frontend/movies.html",
+                Collections.emptyList()
+                );
+    }
+
+    //http://localhost:8080/v2/api-docs
+    //http://localhost:8080/swagger-ui.html
 }
--- src/main/java/pl/edu/pwsztar/controller/MovieApiController.java
+++ src/main/java/pl/edu/pwsztar/controller/MovieApiController.java
@@ -1,5 +1,7 @@
 package pl.edu.pwsztar.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +28,7 @@ public class MovieApiController {
     }
 
     @CrossOrigin
+    @ApiOperation(value = "Find all movies", notes = "provide information about all movies")
     @GetMapping(value = "/movies")
     public ResponseEntity<List<MovieDto>> getMovies() {
         LOGGER.info("find all movies");
@@ -35,6 +38,7 @@ public class MovieApiController {
     }
 
     @CrossOrigin
+    @ApiOperation(value = "Create new movie", notes = "add new movie do collection")
     @PostMapping(value = "/movies")
     public ResponseEntity<?> createMovie(@RequestBody CreateMovieDto createMovieDto) {
         LOGGER.info("create movie: {}", createMovieDto);
@@ -44,8 +48,9 @@ public class MovieApiController {
     }
 
     @CrossOrigin
+    @ApiOperation(value = "Delete movie by id", notes = "delete movie giving id as argument")
     @DeleteMapping(value = "/movies/{movieId}")
-    public ResponseEntity<Void> deleteMovie(@PathVariable Long movieId) {
+    public ResponseEntity<Void> deleteMovie(@ApiParam(value = "unique id of movie") @PathVariable Long movieId) {
         LOGGER.info("delete movie: {}", movieId);
         movieService.deleteMovie(movieId);
 
@@ -53,8 +58,9 @@ public class MovieApiController {
     }
 
     @CrossOrigin
+    @ApiOperation(value = "Find movie by id", notes = "provide information about movie details by id")
     @GetMapping(value = "/movies/{movieId}")
-    public ResponseEntity<DetailsMovieDto> detailsMovie(@PathVariable Long movieId) {
+    public ResponseEntity<DetailsMovieDto> detailsMovie(@ApiParam(value = "unique id of movie", example = "1") @PathVariable Long movieId) {
         LOGGER.info("details movie: {}", movieId);
         DetailsMovieDto detailsMovieDto = movieService.findMovie(movieId);
 
@@ -62,6 +68,7 @@ public class MovieApiController {
     }
 
     @CrossOrigin
+    @ApiOperation(value = "Edit information about movie", notes = "edit information about movie giving its id")
     @PutMapping(value = "/movies/{movieId}")
     public ResponseEntity<Void> updateMovie(@RequestBody UpdateMovieDto updateMovieDto,
                                             @PathVariable Long movieId) {
@@ -72,6 +79,7 @@ public class MovieApiController {
     }
 
     @CrossOrigin
+    @ApiOperation(value = "Count all available movies", notes = "number of all available movies")
     @GetMapping(value = "/movies/counter")
     public ResponseEntity<MovieCounterDto> countMovies() {
         LOGGER.info("count movies");
